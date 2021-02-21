66c35455f78bfc7877592e270f1348d46cdfb5de
magowntown <amagown@gmail.com>
2021-02-21 16:19:49 -0500

    API Security added

--- server/src/config.js
+++ server/src/config.js
@@ -5,6 +5,7 @@ import getDatabaseUrl from "./config/getDatabaseUrl.cjs";
 export default {
   nodeEnv: getNodeEnv(),
   session: { secret: process.env.SESSION_SECRET },
+  unogsKey: { secret: process.env.UNOGS_API_KEY },
   databaseUrl: getDatabaseUrl(getNodeEnv()),
   web: { host: process.env.HOST || "0.0.0.0", port: process.env.PORT || 3000 }
 };
